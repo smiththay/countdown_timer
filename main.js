0be19b713b50bebb3655551dbbffe40cddeae1bd
Smith Thay <contact.smith.thay@gmail.com>
2021-02-19 09:31:43 -0500

    functional code but not complete

---
+++ main.js
@@ -0,0 +1,43 @@
+const finalDate = new Date ("Feb 22, 2021 08:00:00");
+const display = document.getElementById('display')
+
+
+timer = function () {
+    const now = new Date();
+    const diff = finalDate - now;
+    
+
+    let days = Math.floor(diff/ (86400000))
+    let hours = Math.floor(diff %(1000*60*60*24)/(1000*60*60));
+    let minutes = Math.floor((diff % (1000*60*60))/(1000*60));
+    let seconds = Math.floor((diff % (1000*60))/1000);
+    
+    if(days < 9){
+        days = '0' + days
+        }
+        if(days < 99){
+        days = '0' + days
+        }
+        if(hours < 10){
+        hours = '0' + hours
+        }
+        if(minutes < 10){
+        minutes = '0' + minutes
+        }
+        if(seconds < 10){
+        seconds = '0' + seconds 
+        }
+
+    document.querySelector("#days .box").textContent= days;
+    document.querySelector("#hours .box").textContent= hours;
+    document.querySelector("#minutes .box").textContent= minutes;
+    document.querySelector("#seconds .box").textContent= seconds;
+
+    
+}
+
+
+
+setInterval(timer, 1000)
+timer();
+
